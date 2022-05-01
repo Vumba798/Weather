b428153872aac1a1799c1b3eb5afd7a422cbc30d
Vumba798 <alexandrov32649@gmail.com>
2022-05-01 20:44:42 +0300

    changed default colors and theme

--- app/src/main/java/com/example/weather/ui/theme/Color.kt
+++ app/src/main/java/com/example/weather/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.weather.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Blue = Color(0xFFBBDEFB)
+val LightBlue = Color(0xFFEEFFFF)
+val DarkBlue = Color(0xFF8AACC8)
+val Grey = Color(0xFFBDBDBD)
